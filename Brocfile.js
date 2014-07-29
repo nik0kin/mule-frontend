8924392f9ae576d04f05313f363482f484246755
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2014-07-28 23:24:32 -0500

    updated header

--- Brocfile.js
+++ Brocfile.js
@@ -1,4 +1,5 @@
 /* global require, module */
+// %import
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
@@ -16,5 +17,8 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+
+app.import('vendor/bootstrap/dist/css/bootstrap.css');
+app.import('vendor/bootstrap/dist/css/bootstrap-theme.css');
 
 module.exports = app.toTree();
