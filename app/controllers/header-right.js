2934068359692f7e5fa851cf27f463185db99124
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2014-08-02 01:48:47 -0500

    added almost working login

--- app/controllers/header-right.js
+++ app/controllers/header-right.js
@@ -3,7 +3,45 @@ import GamesController from './games';
 
 var HeaderRightController = Ember.Controller.extend({
   //contentBinding: GamesController
-  alert: false
+  loginFailed: false,
+  isProcessing: false,
+  isSlowConnection: false,
+  timeout: null,
+
+  login: function() {
+    this.setProperties({
+      loginFailed: false,
+      isProcessing: true
+    });
+
+    this.set("timeout", setTimeout(this.slowConnection.bind(this), 5000));
+
+    var request = Ember.$.post("http://localhost:3130/LoginAuth", this.getProperties("username", "password"));
+    request.then(this.success.bind(this), this.failure.bind(this));
+    console.log('log in');
+  },
+
+  success: function() {
+    this.reset();
+    //document.location = "/welcome";
+  },
+
+  failure: function() {
+    this.reset();
+    this.set("loginFailed", true);
+  },
+
+  slowConnection: function() {
+    this.set("isSlowConnection", true);
+  },
+
+  reset: function() {
+    clearTimeout(this.get("timeout"));
+    this.setProperties({
+      isProcessing: false,
+      isSlowConnection: false
+    });
+  }
 });
 
 export default HeaderRightController;
