0a1d0d0e2b1c5cdbf492f22645dba385e56cd446
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2014-08-05 23:50:53 -0500

    part of nginx config

--- Brocfile.js
+++ Brocfile.js
@@ -18,7 +18,8 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('vendor/bootstrap/dist/css/bootstrap.css');
-app.import('vendor/bootstrap/dist/css/bootstrap-theme.css');
+//app.import('vendor/bootstrap/dist/css/bootstrap.css');
+//app.import('vendor/bootstrap/dist/css/bootstrap-theme.css');
+//app.import('vendor/bootstrap/dist/css/bootstrap-theme.css.map');
 
 module.exports = app.toTree();
