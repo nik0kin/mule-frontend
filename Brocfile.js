c0c8ba6c7c6346bbfd34a276514c80991c50d148
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2014-10-04 12:14:22 -0500

    using bower for bootstrap/lodash

--- Brocfile.js
+++ Brocfile.js
@@ -17,15 +17,4 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-
-//app.import('vendor/bootstrap/dist/css/bootstrap.css');
-//app.import('vendor/bootstrap/dist/css/bootstrap-theme.css');
-//app.import('vendor/bootstrap/dist/css/bootstrap-theme.css.map');
-
-app.import('vendor/bootstrap/dist/css/bootstrap.css');
-app.import('vendor/bootstrap/dist/js/bootstrap.js');
-
-app.import('vendor/lodash/dist/lodash.js');
-
-
 module.exports = app.toTree();
